a055a52945488e7698d1cde31ec633582ff09693
gribVolnushka <burgerqueen98@gmail.com>
2024-08-26 19:13:54 +0200

    added command alert for conditions branch

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ let result;
 if (firstNumber) {
   if (secondNumber) {
     result = firstNumber + secondNumber;
+    alert(`The result of the calculation is ${result}.`);
   } else {
     alert('Invalid second number');
   }
